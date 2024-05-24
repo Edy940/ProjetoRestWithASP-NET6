65e2e5e7752ea6f2b0c8a89ed5323610433a9f83
Edy940 <edwandersilva@gmail.com>
2024-05-23 23:47:23 -0300

    Testes realizados no Postman, criação dos verbos

---
+++ 01_RestWithASPNET_Scaffold/RestWithASP-NET/RestWithASP-NET/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,63 @@
+﻿using RestWithASP_NET.Model;
+
+namespace RestWithASP_NET.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+           
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for(int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Adalto" + i,
+                LastName = "Alves" + i,
+                Address = "Pernambuco" + i,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment (ref count);
+        }
+
+        public Person FindByID(long id)
+        {
+            return new Person { Id = IncrementAndGet(),
+
+                FirstName = "EdWander",
+                LastName = "Silva",
+                Address = "São Paulo Capital",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+    }
+}
